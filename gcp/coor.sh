0d39b8188d3c8eba874b81d4b678a74ff058f8df
erictaur <erictaur@outlook.com>
2022-02-02 18:45:49 -0500

    - Update run script and logs

--- gcp/coor.sh
+++ gcp/coor.sh
@@ -19,3 +19,69 @@ export REPOSITORY_ORG=idea-fasoc \
 ./config.sh --url https://github.com/$REPOSITORY_ORG/$REPOSITORY_NAME \
             --token $TOKEN \
             --num $SLOTS
+
+# Run logs
+
+# VM setup file
+# {
+#   "gcp": {
+#     "type": "n2-standard-4",
+#     "disk_type": "pd-ssd",
+#     "zone": "us-west1-a",
+#     "project": "catx-ext-umich",
+#     "subnet": "gha-runner-net",
+#     "image": "scalenode-62142bf",
+#     "sa": "gha-runner-coordinator-sa@catx-ext-umich.iam.gserviceaccount.com"
+#   },
+#   "machine": {
+#     "disk": 30,
+#     "preemptible": false
+#   }
+# }
+
+# runner@gha-runner-coordinator:~/github-actions-runner$ supervisord -n -c supervisord.conf
+# 2022-02-02 23:34:13,073 INFO supervisord started with pid 11935
+# 2022-02-02 23:34:14,076 INFO spawned: '0_runner' with pid 11938
+# 2022-02-02 23:34:14,077 INFO spawned: 'unbound' with pid 11939
+# 2022-02-02 23:34:14,079 INFO spawned: 'registry' with pid 11940
+# 2022-02-02 23:34:14,080 INFO spawned: 'runnerversion_cache' with pid 11942
+# 2022-02-02 23:34:14,082 INFO spawned: 'rsyslogd' with pid 11945
+# 2022-02-02 23:34:15,681 INFO success: 0_runner entered RUNNING state, process has stayed up for > than 1 seconds (startsecs)
+# 2022-02-02 23:34:15,681 INFO success: unbound entered RUNNING state, process has stayed up for > than 1 seconds (startsecs)
+# 2022-02-02 23:34:15,681 INFO success: registry entered RUNNING state, process has stayed up for > than 1 seconds (startsecs)
+# 2022-02-02 23:34:15,681 INFO success: runnerversion_cache entered RUNNING state, process has stayed up for > than 1 seconds (startsecs)
+# 2022-02-02 23:34:15,681 INFO success: rsyslogd entered RUNNING state, process has stayed up for > than 1 seconds (startsecs)
+
+# runner@gha-runner-coordinator:~/github-actions-runner$ ./config.sh --url https://github.com/$REPO_ORG/$REPO_NAME --token $TOKEN --num $SLOTS
+# ldd: ./bin/System.Security.Cryptography.Native.OpenSsl.so: No such file or directory
+# ldd: ./bin/System.IO.Compression.Native.so: No such file or directory
+
+# Caught an exception when requesting the Runner Version:
+# Message :Connection refused (localhost:15789) 
+# Version Provider returned version: 2.287.1
+
+# Caught an exception when requesting the Runner Version:
+# Message :Connection refused (localhost:15789) 
+# Version Provider returned version: 2.287.1
+
+# --------------------------------------------------------------------------------
+# |        ____ _ _   _   _       _          _        _   _                      |
+# |       / ___(_) |_| | | |_   _| |__      / \   ___| |_(_) ___  _ __  ___      |
+# |      | |  _| | __| |_| | | | | '_ \    / _ \ / __| __| |/ _ \| '_ \/ __|     |
+# |      | |_| | | |_|  _  | |_| | |_) |  / ___ \ (__| |_| | (_) | | | \__ \     |
+# |       \____|_|\__|_| |_|\__,_|_.__/  /_/   \_\___|\__|_|\___/|_| |_|___/     |
+# |                                                                              |
+# |                       Self-hosted runner registration                        |
+# |                                                                              |
+# --------------------------------------------------------------------------------
+
+# # Authentication
+# √ Connected to GitHub
+# # Runner Registration
+# Caught an exception when requesting the Runner Version:
+# Message :Connection refused (localhost:15789) 
+# Version Provider returned version: 2.287.1
+# √ Runner successfully added
+# √ Runner connection is good
+# # Runner settings
+# √ Settings Saved.
--- gcp/instance.sh
+++ gcp/instance.sh
@@ -1,72 +1,85 @@
 #! /bin/bash
 
-# Setup prerequisites
-sudo apt-get update
-sudo apt install build-essential -y
-sudo apt-get -qqy install file gcc-multilib mtools \
-    automake autoconf build-essential bison flex \
-    wget patch bc unzip make rsync binutils cpio git \
-    libelf-dev python3 golang fdisk dosfstools texinfo 
+START_UP=/tmp/startup
+if test -f "$START_UP"; then
+    echo "Initialization already finished."
+else
+    echo "Initialization not present; Installing prerequesties"
 
-# Install Go for compiling BzImage
-# cd ~
-# wget https://golang.org/dl/go1.16.5.linux-amd64.tar.gz && \
-#     sudo tar -xzf go1.16.5.linux-amd64.tar.gz -C /usr/local/ && \
-#     export PATH=$PATH:/usr/local/go/bin
+    # Setup prerequisites
+    sudo apt-get update
+    sudo apt-get -qqy install file gcc-multilib mtools \
+        automake autoconf build-essential bison flex \
+        wget patch bc unzip make rsync binutils cpio git \
+        libelf-dev python3 golang fdisk dosfstools texinfo 
 
-# Install Terraform
-sudo apt-get install software-properties-common -y
-curl -fsSL https://apt.releases.hashicorp.com/gpg | 
-    sudo apt-key add -
-sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-sudo apt update && sudo apt install terraform -y
+    # Install Go for compiling BzImage
+    # cd ~
+    # wget https://golang.org/dl/go1.16.5.linux-amd64.tar.gz && \
+    #     sudo tar -xzf go1.16.5.linux-amd64.tar.gz -C /usr/local/ && \
+    #     export PATH=$PATH:/usr/local/go/bin
 
-# Install Google Cloud SDK
-echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | 
-    sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+    # Install Terraform
+    sudo apt-get install software-properties-common -y
+    curl -fsSL https://apt.releases.hashicorp.com/gpg | \
+        sudo apt-key add -
+    sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+    sudo apt update && sudo apt install terraform -y
 
-sudo apt-get install apt-transport-https ca-certificates gnupg -y
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | 
-    sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+    # Install Google Cloud SDK
+    echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | \
+        sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 
-sudo apt-get update && sudo apt-get install google-cloud-sdk -y
+    sudo apt-get install apt-transport-https ca-certificates gnupg -y
+    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | \
+        sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 
-# Remove all created resources by Terraform
-yes | gsutil rm gs://catx-ext-umich-worker-bucket/scalenode-62142bf.tar.gz
-yes | gcloud compute images delete scalenode-62142bf
-yes | gcloud compute instances delete gha-runner-coordinator --zone=us-west1-a
-yes | gcloud compute firewall-rules delete gha-runner-net---ssh-from-outside \
-        gha-runner-net---deny-access-coordinator \
-        gha-runner-net---access-runners \
-        gha-runner-net---allow-coordinator-services
-yes | gcloud compute routers delete gha-runner-net---cloud-router \
-        --project=catx-ext-umich \
-        --region=us-west1
-yes | gcloud compute networks subnets delete gha-runner-net --region=us-west1
-yes | gcloud compute networks delete gha-runner-net
-yes | gcloud iam service-accounts delete gha-runner-coordinator-sa@catx-ext-umich.iam.gserviceaccount.com
-yes | gcloud compute disks delete gha-runner-coordinator---boot-disk --zone=us-west1-a
+    sudo apt-get update && sudo apt-get install google-cloud-sdk -y
 
-# Rebuild image
-cd /tmp && \
-    git clone --recursive \
-    https://github.com/antmicro/github-actions-runner-scalerunner.git && \
-        cd github-actions-runner-scalerunner/buildroot && \
-        make BR2_EXTERNAL=../overlay/ scalenode_gcp_defconfig && \
-        ( FORCE_UNSAFE_CONFIGURE=1 make -j$(nproc) 2>&1 | tail -10000 ) || FORCE_UNSAFE_CONFIGURE=1 make
+    # Remove all created resources by Terraform
+    yes | gsutil rm gs://catx-ext-umich-worker-bucket/scalenode-62142bf.tar.gz
+    yes | gcloud compute images delete scalenode-62142bf
+    yes | gcloud compute instances delete gha-runner-coordinator --zone=us-west1-a
+    yes | gcloud compute firewall-rules delete gha-runner-net---ssh-from-outside \
+            gha-runner-net---deny-access-coordinator \
+            gha-runner-net---access-runners \
+            gha-runner-net---allow-coordinator-services
+    yes | gcloud compute routers delete gha-runner-net---cloud-router \
+            --project=catx-ext-umich \
+            --region=us-west1
+    yes | gcloud compute networks subnets delete gha-runner-net --region=us-west1
+    yes | gcloud compute networks delete gha-runner-net
+    yes | gcloud iam service-accounts delete gha-runner-coordinator-sa@catx-ext-umich.iam.gserviceaccount.com
+    yes | gcloud compute disks delete gha-runner-coordinator---boot-disk --zone=us-west1-a
 
-export PROJECT=catx-ext-umich && \
-    export BUCKET=$PROJECT-worker-bucket
+    # Rebuild image
+    cd /tmp && \
+        git clone --recursive \
+        https://github.com/antmicro/github-actions-runner-scalerunner.git && \
+            cd github-actions-runner-scalerunner/buildroot && \
+            make BR2_EXTERNAL=../overlay/ scalenode_gcp_defconfig && \
+            ( FORCE_UNSAFE_CONFIGURE=1 make -j$(nproc) 2>&1 | tail -10000 ) || FORCE_UNSAFE_CONFIGURE=1 make
 
-# Make and upload image
-cd ../ && \
-    ./make_gcp_image.sh && \
-    ./upload_gcp_image.sh $PROJECT $BUCKET
+    export PROJECT=catx-ext-umich && \
+        export BUCKET=$PROJECT-worker-bucket
 
-# Run Terraform with plan file
-cd /tmp && \
-    git clone https://github.com/antmicro/github-actions-runner-terraform.git && \
-    cd github-actions-runner-terraform
+    # Make and upload image
+    cd ../ && \
+        ./make_gcp_image.sh && \
+        ./upload_gcp_image.sh $PROJECT $BUCKET
 
-echo 'catx-ext-umich' | terraform plan && \
+    # Run Terraform with plan file
+    cd /tmp && \
+        git clone https://github.com/antmicro/github-actions-runner-terraform.git && \
+        cd github-actions-runner-terraform
+
+    echo 'catx-ext-umich' | terraform init
     echo 'catx-ext-umich' | terraform apply -auto-approve
+
+    # Create startup checkpoint
+    touch /tmp/startup
+    echo "Initialization completed!"
+
+fi
+
+gcloud compute ssh gha-runner-coordinator --zone=us-west1-a
